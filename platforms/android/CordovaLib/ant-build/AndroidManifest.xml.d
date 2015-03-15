e87687e1c8525b3f869f31bf42539a4285f1fc52
shoukathayat222 <shoukathayat222@gmail.com>
2015-03-16 00:10:55 +0500

    changes
    test

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Applications/XAMPP/xamppfiles/htdocs/PushNotificaitonSample/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Applications/XAMPP/xamppfiles/htdocs/PushNotificaitonSample/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Applications/XAMPP/xamppfiles/htdocs/PushNotificaitonSample/platforms/android/ant-build/AndroidManifest.xml : \
-/Applications/XAMPP/xamppfiles/htdocs/PushNotificaitonSample/platforms/android/AndroidManifest.xml \
